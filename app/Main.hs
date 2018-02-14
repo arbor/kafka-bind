71fbce3c6495919a36587dc51586e21b52c5e1cf
John Ky <newhoggy@gmail.com>
2018-02-14 19:44:56 +1100

    Extract common code

--- app/Main.hs
+++ app/Main.hs
@@ -10,32 +10,41 @@ import App.Options.Parser
 import App.RunApplication
 import Arbor.Logger
 import Control.Lens
+import Control.Monad.IO.Class
 import Data.Semigroup                       ((<>))
 import HaskellWorks.Data.Conduit.Combinator
 import Kafka.Avro                           (schemaRegistry)
 import Kafka.Conduit.Source
+import Network.AWS.Env
 import Network.StatsD                       as S
 import System.Environment
 
 import qualified App.Action.KafkaToSqs as C
 import qualified Data.Text             as T
 
+mkAppEnv :: RunApplication c => StatsClient -> Logger -> Env -> GlobalOptions c -> c -> AppEnv c
+mkAppEnv stats logger envAws opt cmd =
+  let newOpt = opt { _optCmd = cmd }
+      envApp = AppEnv newOpt envAws stats logger
+  in envApp
+
 main :: IO ()
 main = do
   opt <- parseOptions
   progName <- T.pack <$> getProgName
-  let logLvk  = opt ^. optLogLevel
+  let logLvk    = opt ^. optLogLevel
   let statsConf = opt ^. optStatsConfig
 
   withStdOutTimedFastLogger $ \lgr -> do
     withStatsClient progName statsConf $ \stats -> do
       envAws <- mkEnv (opt ^. optRegion) logLvk lgr
-      case opt ^. optCmd of
-        CmdOfCmdKafkaToSqs cmd -> do
-          let newOpt = opt { _optCmd = cmd }
-          let envApp = AppEnv newOpt envAws stats (Logger lgr logLvk)
-          res <- runApplication envApp
-          case res of
-            Left err -> pushLogMessage lgr LevelError ("Exiting: " <> show err)
-            Right _  -> pure ()
+      let mkAppEnv2 :: RunApplication c => c -> AppEnv c
+          mkAppEnv2 cmd = mkAppEnv stats (Logger lgr logLvk) envAws (opt { _optCmd = cmd }) cmd
+      res <- case opt ^. optCmd of
+        CmdOfCmdKafkaToSqs cmd -> runApplication (mkAppEnv2 cmd)
+        CmdOfCmdSqsToKafka cmd -> runApplication (mkAppEnv2 cmd)
+        cmd                    -> return $ Left (AppErr ("Not implemented: " <> show cmd))
+      case res of
+        Left err -> pushLogMessage lgr LevelError ("Exiting: " <> show err)
+        Right _  -> pure ()
     pushLogMessage lgr LevelError ("Premature exit, must not happen." :: String)
--- src/App/Options/Cmd/SqsToKafka.hs
+++ src/App/Options/Cmd/SqsToKafka.hs
@@ -7,12 +7,46 @@ module App.Options.Cmd.SqsToKafka
   , parserCmdSqsToKafka
   ) where
 
+import App
+import App.AWS.Sqs
+import App.Kafka
+import App.RunApplication
+import Arbor.Logger
+import Conduit
+import Control.Arrow                        (left)
+import Control.Concurrent
+import Control.Exception
 import Control.Lens
+import Control.Monad.Except
+import Data.Aeson                           as J
+import Data.ByteString                      (ByteString)
+import Data.ByteString.Char8                as C8
+import Data.ByteString.Lazy                 (fromStrict, toStrict)
+import Data.Maybe                           (catMaybes)
+import Data.Monoid
+import HaskellWorks.Data.Conduit.Combinator
+import Kafka.Avro
+import Kafka.Conduit.Sink
+import Kafka.Conduit.Source
+import Network.StatsD                       as S
 import Options.Applicative
+
+import qualified Data.Avro.Decode  as A
+import qualified Data.Avro.Schema  as A
+import qualified Data.Avro.Types   as A
+import qualified Data.Conduit.List as L
+import qualified Data.Text         as T
+import qualified System.IO         as P
 
 data CmdSqsToKafka = CmdSqsToKafka deriving (Show, Eq)
 
 makeLenses ''CmdSqsToKafka
+
+instance RunApplication CmdSqsToKafka where
+  runApplication envApp = runApplicationM envApp $ do
+    liftIO $ P.putStrLn "Not yet implemented"
+    forever $ liftIO $ threadDelay 1000
+    return ()
 
 parserCmdSqsToKafka :: Parser CmdSqsToKafka
 parserCmdSqsToKafka = pure CmdSqsToKafka
