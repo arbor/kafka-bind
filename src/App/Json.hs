36de487cf79a15dde8f4c7f93913e8f07610155f
Jian Wan <jwan@arbor.net>
2018-09-20 08:59:15 +1000

    MAYH-9842 unescape objectkey from sns notification

--- src/App/Json.hs
+++ src/App/Json.hs
@@ -17,15 +17,11 @@ import Data.Monoid
 import qualified Antiope.Contract.SQS.FileChangeMessage as SQSM
 import qualified Antiope.Contract.SQS.ResourceChanged   as SQSM
 import qualified Data.Aeson                             as J
-import qualified Data.Text                              as T
-import qualified Network.URI                            as URI
 
 fileChangeMessageToResourceChanged :: Monad m => J.Value -> ExceptT AppError m J.Value
 fileChangeMessageToResourceChanged v = case J.fromJSON v of
   J.Success (fcm :: SQSM.FileChangeMessage) -> return $ J.toJSON $ SQSM.ResourceChanged
     { SQSM.eventTime  = fcm ^. the @"eventTime"
-    , SQSM.uri        = "s3://" <> (fcm ^. the @"bucketName") <> "/" <> (unescape $ fcm ^. the @"objectKey")
+    , SQSM.uri        = "s3://" <> (fcm ^. the @"bucketName") <> "/" <> fcm ^. the @"objectKey"
     }
   J.Error msg                               -> throwError (AppErr msg)
-  where
-    unescape = T.pack . URI.unEscapeString . T.unpack
